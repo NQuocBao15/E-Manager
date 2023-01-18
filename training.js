1da99c5ec12a20bb3b7a079c12604607620f7766
NQuocBao <nguyenquocbao9483@gmail.com>
2023-01-18 08:19:33 +0700

    update

--- training.js
+++ training.js
@@ -1,11 +1,11 @@
 class Employee {
-    constructor(id, name, courseName, courseProvider, daysWorked, status) {
+    constructor(id, name, courseName, email, startDay, action) {
         this.id = id;
         this.name = name;
         this.courseName = courseName;
-        this.courseProvider = courseProvider;
-        this.daysWorked = daysWorked;
-        this.status = status
+        this.email = email;
+        this.startDay = startDay;
+        this.action = action
     }
 }
 
@@ -16,11 +16,12 @@ const employee_data = "employee_data";
 function init() {
     if (localStorage.getItem(employee_data) == null) {
         employees = [
-            new Employee(1, 'Roger', 'Java', 'ABC Tech', 12, 'Pending'),
-            new Employee(2, 'Mike', 'PHP', 'ABC Tech', 7, 'Pending'),
-            new Employee(3, 'John', 'C++', 'ABC Tech', 9, 'Pending'),
-            new Employee(4, 'Anna', 'Java', 'ABC Tech', 15, 'Pending'),
-            new Employee(5, 'Phat', 'PHP', 'ABC Tech', 8, 'Pending'),
+            new Employee(1, 'Roger', 'Java', 'roger@gmail.com', '2023-01-06'),
+            new Employee(2, 'Dove', 'PHP', 'dove@gmail.com', '2021-05-18'),
+            new Employee(3, 'John', 'C++', 'john@gmail.com', '2022-09-10'),
+            new Employee(4, 'HachCo', 'C++', 'hachco@gmail.com', '2022-01-15'),
+            new Employee(5, 'Mike', 'Java', 'mike@gmail.com', '2021-03-08'),
+            new Employee(6, 'Phat', 'PHP', 'phat@gmail.com', '2020-06-19'),
         ]
         localStorage.setItem(employee_data, JSON.stringify(employees));
     }
@@ -33,27 +34,36 @@ function renderEmployee() {
     let tbEmployee = document.getElementById('tbodyemp');
     tbEmployee.innerHTML = "";
     for (let i = 0; i < employees.length; i++) {
-        let status = "";
-        if (employees[i].status == 'Pending') {
-            status = `<button class="btn btn-success" onclick="approved(${employees[i].id})">Approved</button>
-                        <button class="btn btn-danger" onclick="reject(${employees[i].id})">Reject</button>`
-        }
         tbEmployee.innerHTML += `
             <tr>
                 <td>${i + 1}</td>
                 <td>${employees[i].name}</td>
                 <td>${employees[i].courseName}</td>
-                <td>${employees[i].courseProvider}</td>
-                <td>${employees[i].daysWorked}</td>
-                <td>${employees[i].status}</td>
-                <td>
-                    ${status}
-                </td>
+                <td>${employees[i].email}</td>
+                <td>${employees[i].startDay}</td>
+                <td><button class="btn edit" onclick="tbEdit(${employees[i].id})">Edit</button><button class="btn delete" onclick="tbDelete(${employees[i].id})">Delete</button></td>
             </tr>
         `;
     }
 }
 
+function submitEmployee() {
+    let name = document.getElementById('nameEmp').value;
+    let courseName = document.getElementById('courseEmp').value;
+    let startDay = document.getElementById('daysEmp').value;
+    let email = document.getElementById('emailEmp').value;
+    if (name == "" || courseName == "" || startDay == "" || email == "") {
+        alert("Please check your information again, you're still missing something.")
+    }
+    else {
+        let id = findMaxId() + 1;
+        let employee = new Employee(id, name, courseName, email, startDay);
+        employees.push(employee);
+        localStorage.setItem(employee_data, JSON.stringify(employees));
+        renderEmployee();
+        clearForm();
+    }
+}
 function findMaxId() {
     let max = 0;
     for (let i = 0; i < employees.length; i++) {
@@ -68,43 +78,36 @@ function clearForm() {
     document.getElementById('nameEmp').value = "";
     document.getElementById('courseEmp').value = "";
     document.getElementById('daysEmp').value = "";
+    document.getElementById('emailEmp').value = "";
+
 }
 
-function submitEmployee() {
-    let name = document.getElementById('nameEmp').value;
-    let courseName = document.getElementById('courseEmp').value;
-    let daysWorked = document.getElementById('daysEmp').value;
-    if (name == "" || courseName == "" || daysWorked == "") {
-        alert("You need provide data.")
-    }
-    else {
-        let id = findMaxId() + 1;
-        let employee = new Employee(id, name, courseName, "ABC Tech", daysWorked, 'Pending');
-        employees.push(employee);
-        localStorage.setItem(employee_data, JSON.stringify(employees));
-        renderEmployee();
-        clearForm();
-    }
+function tbEdit(employeesID) {
+    let employees = findEmployeeById(employeesID);
+    document.getElementById('nameEmp').value = employees.name;
+    document.getElementById('courseEmp').value = employees.courseName;
+    document.getElementById('daysEmp').value = employees.startDay;
+    document.getElementById('emailEmp').value = employees.email;
+    localStorage.setItem(employee_data, JSON.stringify(employees));
+    renderEmployee();
 }
 
-function approved(employeeID) {
-    let confirm = window.confirm("Are you sure to approve this employee?");
+function tbDelete(employeesID) {
+    let confirm = window.confirm("Are you sure to delete this employee?");
     if (confirm == true) {
-        let employee = findEmployeeById(employeeID);
-        employee.status = "Approved";
+        let index = findIndexEmployeeById(employeesID);
+        employees.splice(index, 1);
         localStorage.setItem(employee_data, JSON.stringify(employees));
         renderEmployee();
     }
 }
-
-function reject(employeeID) {
-    let confirm = window.confirm("Are you sure to reject this employee?");
-    if (confirm == true) {
-        let employee = findEmployeeById(employeeID);
-        employee.status = "Reject";
-        localStorage.setItem(employee_data, JSON.stringify(employees));
-        renderEmployee();
+function findIndexEmployeeById(employeesID) {
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].id == employeesID) {
+            return i;
+        }
     }
+    return -1;
 }
 
 function findEmployeeById(employeesID) {
@@ -115,6 +118,8 @@ function findEmployeeById(employeesID) {
     }
     return null;
 }
+
+
 
 init();
 renderEmployee()
